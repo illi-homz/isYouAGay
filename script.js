3f55bd527fae9773a289b3e2abb6d88c37e7e2a8
Ilya Gomza <54039668+illi-homz@users.noreply.github.com>
2023-01-11 01:52:35 +0300

    fix

--- script.js
+++ script.js
@@ -24,7 +24,7 @@ const main = () => {
         titleNode.classList.remove('hidden')
         loaderNode.classList.add('hidden')
 
-        window.location.replace("http://ya.ru");
+        // window.location.replace("http://ya.ru");
     }, 3000)
 };
 
